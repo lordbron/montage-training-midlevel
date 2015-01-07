2e701b5474a436d1d430fd6890d49e67a000327e
lordbron <noreply>
2015-01-07 15:51:18 +0000

    Project Assignment 9 End Point/Project Assignment 10 Start Point

---
+++ ui/storm-list-item.reel/storm-list-item.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/storm-list-item.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class StormListItem
+ * @extends Component
+ */
+exports.StormListItem = Component.specialize(/** @lends StormListItem# */ {
+    constructor: {
+        value: function StormListItem() {
+            this.super();
+        }
+    }
+});
