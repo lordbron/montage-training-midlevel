439b10e0155a64f95bd3e0b43600d17486bbf9f8
lordbron <noreply>
2015-01-07 12:49:25 +0000

    Project Assignment 8 End Point/Project Assignment 9 Start Point

--- ui/default.reel/default.js
+++ ui/default.reel/default.js
@@ -13,5 +13,17 @@ exports.Default = Component.specialize(/** @lends Default# */ {
         value: function Default() {
             this.super();
         }
+    },
+    enterDocument: {
+        value: function (firstTime) {
+            if (firstTime) {
+                this.templateObjects.stormController.content = [
+                    {"name":"Wilma", "type":"Tropical Storm"},
+                    {"name":"Fred", "type":"Hurricane"},
+                    {"name":"Betty", "type":"Tropical Storm"},
+                    {"name":"Barney", "type":"Hurricane"}
+                ];
+            }
+        }
     }
 });
